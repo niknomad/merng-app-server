d0707672491485fd2d224ef91ee29dc2b7c5c0cf
nikhil <nikgarg01@gmail.com>
2020-11-16 22:04:04 +0530

    init

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGOURL: 'mongodb+srv://Nikhil:qwertyuiop1234567890@cluster0.syd9n.mongodb.net/dailyMeet?retryWrites=true&w=majority',
+    SECRET_KEY: 'encode key'
+}
+// https://cloud.mongodb.com/v2/5f9f082423dad96fb9f81df0#clusters
